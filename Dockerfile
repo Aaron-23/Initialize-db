81faeff67e3dce78003a828b1a6c724bf624a5ef
Aaron-23 <851882108@qq.com>
2020-11-16 16:23:36 +0800

    INI

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,8 @@ MAINTAINER Aaron <Aaron_ops@163.com>
 #把sql文件拷贝到/docker-entrypoint-initdb.d/目录下，以便启动时自动执行这个sql
 COPY ./sql/*.sql /docker-entrypoint-initdb.d
 
+#Mysql密码
+ENV MYSQL_ROOT_PASSWORD rainbond
 #数据持久化目录
 VOLUME [ "/var/lib/mysql" ]
 #端口
--- sql/init_database.sql
+++ sql/init_database.sql
@@ -9,6 +9,12 @@ CREATE TABLE IF NOT EXISTS `role_info` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(32) NOT NULL DEFAULT '',
   `kind_id` varchar(64) NOT NULL DEFAULT '',
-  `kind` varchar(32) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=96 DEFAULT CHARSET=utf8;
+
+-- 插入数据
+INSERT INTO `role_info` (`id`, `name`, `kind_id`)
+VALUES
+(1,'user01',10),
+(2,'user02',10),
+(3,'user03',10);
