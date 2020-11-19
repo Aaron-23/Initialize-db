17b48255e9f30058d968a90b7fb720feda909d19
Aaron-23 <851882108@qq.com>
2020-11-19 13:52:13 +0800

    update

--- upgrade/5.1-5.2.sql
+++ upgrade/5.1-5.2.sql
@@ -2,8 +2,4 @@
 use rainbond;
 
 -- 插入数据
-INSERT INTO `role_info` (`id`, `name`, `kind_id`)
-VALUES
-(4,'user04',20),
-(5,'user05',20),
-(6,'user06',20);
+ALTER TABLE role_info ADD `kind_id` varchar(64) NOT NULL DEFAULT;
