85329ba4bef02f8d299968f017da47462d8fa3a6
Aaron-23 <851882108@qq.com>
2020-11-19 14:32:09 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,9 +7,7 @@ mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "select version();" &> /dev/null
 
 #导入数据
 echo 'Perform upgrade sql......'
-mysql -uroot -p$MYSQL_ROOT_PASSWORD
-source /upgrade/5.1-5.2.sql;
+mysql -uroot -p$MYSQL_ROOT_PASSWORD < /upgrade/5.1-5.2.sql
 echo 'Upgrade sql execution completed'
 
-tail -f /dev/null
 
